3c72337fcf7ded7dcafc2686489750fc81459a06
Da Ziang <daziang@microsoft.com>
2022-09-20 11:13:32 -0700

    minor adjustment

--- src/Components/TrivialTemplateEngine/ChapterCreation.tsx
+++ src/Components/TrivialTemplateEngine/ChapterCreation.tsx
@@ -1,9 +1,10 @@
 import { Stack } from "@fluentui/react";
 import { useState } from "react";
 import ChapterComponent from "./ChapterComponent";
+import PageCreation from "./PageCreation";
 import { Chapter } from "./TrivialTemplateModel";
 
-export default function ChapterCreation(){
+export default function ChapterCreation(props:{chapters:Chapter[]}){
 
     const [Chapters, setChapter] = useState([ChapterComponent()]);
 
@@ -18,8 +19,19 @@ export default function ChapterCreation(){
     }
     return(
         <Stack>
-            <div>{Chapters}</div>
-            <div>{ChapterCreationMenu}</div>
+            <div className="ms-Grid-row">
+                <div className="ms-Grid-col ms-sm12 ms-md6 ms-lg4">
+                    <div className="LayoutPage-demoBlock">
+                        <div>{Chapters}</div>
+                        <div>{ChapterCreationMenu}</div>
+                    </div>
+                </div>
+                <div className="ms-Grid-col ms-md6 ms-lg8">
+                    <div className="LayoutPage-demoBlock">
+                        <PageCreation pages={[]}/>
+                    </div>
+                </div>
+            </div>
         </Stack>
     )
 }
